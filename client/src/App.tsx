d4655e69d9ead44f0e0d710d88a9100bcc823f53
Ajay Dhakad <144946727+Ajay-Dhakad@users.noreply.github.com>
2024-06-03 19:30:42 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,13 @@ import { Toaster } from 'react-hot-toast'
 import Header from './pages/components/Header'
 import { useSelector, useDispatch } from 'react-redux';
 import { setUserInfo} from './store/userSlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
   const {pathname} = useLocation()
+  const [loading,setloading] = useState<boolean>(false)
   const dispatch = useDispatch()
 
   const user = useSelector((state : any) => state.user.userInfo)
@@ -18,6 +19,7 @@ function App() {
 
 
   const getuserInfo = async() =>{
+    setloading(true)
     const response = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/user/getuser`,{
       method:'GET',
       headers:{
@@ -28,8 +30,10 @@ function App() {
     const data = await response.json()
     if (data.success) {
       dispatch(setUserInfo(data.user))
+      setloading(false) 
     
     }
+    setloading(false)
   } 
 
 useEffect(() => {
@@ -44,7 +48,7 @@ if (!user && localStorage.getItem('auth_token')) getuserInfo();
     <>
     <Header/>
     <Toaster position='top-center'/>
-    <Outlet/>
+    { !loading ?<Outlet/> : <div className='w-full h-full flex justify-center items-center bg-slate-800'><h1 className='text-xl text-white'>Loading...</h1></div> }
     </>
   )
 } 
